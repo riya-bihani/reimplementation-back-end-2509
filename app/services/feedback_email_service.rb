0b4f7b54fdd64a2ccd4d64ca0900eac790f7e655
riya-bihani <riyanbihani@gmail.com>
2025-03-23 19:00:43 -0400

    cleaning up files

--- app/services/feedback_email_service.rb
+++
@@ -1,7 +0,0 @@
-class FeedbackEmailService
-  def self.send_feedback_email(defn)
-    Mailer.sync_message(defn).deliver
-  rescue StandardError => e
-    Rails.logger.error "Error sending feedback email: #{e.message}"
-  end
-end
